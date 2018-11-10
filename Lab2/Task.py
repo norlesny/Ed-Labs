cd5f6ea861eb25fd868d6b8749fc049dd68eec79
norlesny <norlesny@gmail.com>
2018-11-10 15:19:09 +0100

    Solve kNN

--- Lab2/Task.py
+++ Lab2/Task.py
@@ -1,4 +1,31 @@
+from Lab2.solver.DataEntry import DataEntry
 from Lab2.solver.KNNSolver import KNNSolver
 from Lab2.solver.Metric import Metric
 
 solver = KNNSolver(5, Metric.MANHATTAN)
+
+a = [
+    DataEntry([1, 5], 0),
+    DataEntry([2, 6], 0),
+    DataEntry([2, 7], 1),
+    DataEntry([3, 7], 0),
+    DataEntry([3, 8], 1),
+    DataEntry([4, 8], 0),
+    DataEntry([5, 1], 1),
+    DataEntry([5, 9], 0),
+    DataEntry([6, 2], 1),
+    DataEntry([7, 2], 0),
+    DataEntry([7, 3], 1),
+    DataEntry([8, 3], 0),
+    DataEntry([8, 4], 1),
+    DataEntry([9, 5], 1),
+    DataEntry([10, 6], 1)]
+
+b = DataEntry([5, 5])
+
+solver.set_learning_data(a)
+result = solver.classify(b)
+
+print(str(result))
+
+print("Finished")
--- Lab2/solver/KNNSolver.py
+++ Lab2/solver/KNNSolver.py
@@ -1,3 +1,4 @@
+from Lab2.solver.DataEntry import DataEntry
 from Lab2.solver.Metric import Metric
 
 
@@ -6,16 +7,41 @@ class KNNSolver:
     def __init__(self, k, metric) -> None:
         assert type(k) is int
         assert type(metric) is Metric
+
         self._learning_data = []
         self._k = k
         self._metric = metric
 
     def set_learning_data(self, learning_data):
+        assert type(learning_data) is list
+        for entry in learning_data:
+            assert type(entry) is DataEntry
+
         self._learning_data = learning_data
 
+    @staticmethod
+    def take_second(element):
+        return element[1]
+
     def classify(self, data):
+        assert type(data) is DataEntry
+
         #  1. obliczyć odległości pomiędzy learning_data a data
+        distances_map = []
+        for entry in self._learning_data:
+            distances_map.append([entry.classification, data.distance(entry)])
+
         #  2. posortować odległości
+        distances_map.sort(key=KNNSolver.take_second, reverse=True)
+
         #  3. wybrać k elementów z posortowanej liczby
-        #  4. znaleźć najczęstszy wynik z wybranych elementów (jeśli remis -> losowy)
-        pass
+        classifications = {}
+        for entry in distances_map[:self._k]:
+            classification = str(entry[0])
+            if classification in classifications:
+                classifications[classification] += 1
+            else:
+                classifications[classification] = 1
+
+        # 4. znaleźć najczęstszy wynik z wybranych elementów (jeśli remis -> losowy)
+        return max(classifications, key=classifications.get)
