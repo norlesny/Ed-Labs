82799cfd868db39c862846314f5e31ee1c19aec5
norlesny <norlesny@gmail.com>
2018-11-10 23:19:55 +0100

    Parse string to Metric

--- Lab2/solver/Metric.py
+++ Lab2/solver/Metric.py
@@ -4,3 +4,12 @@ from enum import Enum
 class Metric(Enum):
     EUCLIDES = 0
     MANHATTAN = 1
+
+    @staticmethod
+    def from_str(string):
+        assert type(string) is str
+
+        if string == 'euclides':
+            return Metric.EUCLIDES
+        elif string == 'manhattan':
+            return Metric.MANHATTAN
