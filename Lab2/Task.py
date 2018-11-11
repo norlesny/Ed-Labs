52c2eface36258da2e489793eaf4e918f026847a
norlesny <norlesny@gmail.com>
2018-11-11 12:07:34 +0100

    Support testing on training set

--- Lab2/Task.py
+++ Lab2/Task.py
@@ -4,8 +4,9 @@ import sys
 sys.path.append('../')
 
 from Lab2.solver import CsvDataParser
-from Lab2.solver.Metric import Metric
 from Lab2.solver.KNNSolver import KNNSolver
+from Lab2.solver.Metric import Metric
+from Lab2.TestingType import TestingType
 
 parser = argparse.ArgumentParser(description="k-NN algorithm")
 
@@ -14,15 +15,23 @@ parser.add_argument('-k', type=int, default=5, help='the value of k in the algor
 parser.add_argument('-m', type=str, default='euclides', choices=['euclides', 'manhattan'],
                     help='the metric to be used')
 parser.add_argument('-d', type=int, default=-1, help='the index of the classification attribute')
+parser.add_argument('-t', type=str, default='train', choices=['train', 'split', 'cross'],
+                    help='how the testing data set will be chosen')
 
 args = parser.parse_args()
 
 solver = KNNSolver(int(args.__getattribute__('k')), Metric.from_str(args.__getattribute__('m')))
-
 data_set = CsvDataParser.read_data(args.__getattribute__('data_file'))
-solver.set_learning_data(data_set)
 
-result = solver.classify(data_set[100])
-print(result)
+testing_type = TestingType.from_string(args.__getattribute__('t'))
+if testing_type == TestingType.TRAIN:
+    correct_count = 0
+    solver.set_learning_data(data_set)
+    print('class\t\tdata')
+    for entry in data_set:
+        classification = solver.classify(entry)
+        print("{}\t\t{}".format(classification, entry))
+        if classification == entry.classification:
+            correct_count += 1
 
-print("Finished")
+    print('Accuracy: {}%'.format(correct_count / len(data_set) * 100))
