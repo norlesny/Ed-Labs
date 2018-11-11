ae523c41804ba12c7beb2424c6689642c4df9d26
norlesny <norlesny@gmail.com>
2018-11-11 12:43:18 +0100

    Support testing on split data set

--- Lab2/Task.py
+++ Lab2/Task.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 import sys
 
 sys.path.append('../')
@@ -17,6 +18,7 @@ parser.add_argument('-m', type=str, default='euclides', choices=['euclides', 'ma
 parser.add_argument('-d', type=int, default=-1, help='the index of the classification attribute')
 parser.add_argument('-t', type=str, default='train', choices=['train', 'split', 'cross'],
                     help='how the testing data set will be chosen')
+parser.add_argument('-s', type=float, default=0.25, help='percentage of the data to be used as the learning part')
 
 args = parser.parse_args()
 
@@ -35,3 +37,24 @@ if testing_type == TestingType.TRAIN:
             correct_count += 1
 
     print('Accuracy: {}%'.format(correct_count / len(data_set) * 100))
+
+elif testing_type == TestingType.SPLIT:
+    percentage_split = float(args.__getattribute__('s'))
+    # check if percentage_split is >0 and <1
+    testing_set = data_set.copy()
+    learning_set = []
+    number_of_learning_elements = int(len(data_set) * percentage_split)
+    while len(learning_set) < number_of_learning_elements:
+        index = random.randint(0, len(testing_set) - 1)
+        learning_set.append(testing_set.pop(index))
+
+    correct_count = 0
+    solver.set_learning_data(learning_set)
+    print('class\t\tdata')
+    for entry in testing_set:
+        classification = solver.classify(entry)
+        print("{}\t\t{}".format(classification, entry))
+        if classification == entry.classification:
+            correct_count += 1
+
+    print('Accuracy: {}%'.format(correct_count / len(testing_set) * 100))
