f5c96f1bb7a86fa2db2e1441ff021fddfca7af15
norlesny <norlesny@gmail.com>
2018-12-09 09:57:06 +0100

    Calculate entropy

--- Lab3/Task.py
+++ Lab3/Task.py
@@ -1,4 +1,27 @@
+import math
+
 from Lab3 import CsvDataParser
 
-data = CsvDataParser.read_data('test.csv')
+
+def entropy(data):
+    values = {}
+    for entry in data:
+        value = str(entry)
+        if value in values:
+            values[value] += 1
+        else:
+            values[value] = 1
+
+    count = len(data)
+    sum = 0
+    for x in values:
+        p = values[x] / count
+        sum += p * math.log(p, 2)
+
+    return -sum
+
+
+data = CsvDataParser.read_data('test2.csv')
 print(data)
+
+print(entropy([d.classification for d in data]))
